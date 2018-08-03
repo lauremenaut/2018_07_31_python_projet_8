79bbe1f7c3a611bbec3e4d699223b8adea014a2c
Laure <lauremenaut@yahoo.fr>
2018-08-03 16:05:03 -0400

    create params.py file

---
+++ pur_beurre_lm/healthier_food/params.py
@@ -0,0 +1,12 @@
+# Tags for nutrition grades of healthy and unhealthy products
+nutrition_grades = ["a", "b", "d", "e"]
+
+# Tags for (unhealthy) products categories
+tag_categories = ["Céréales pour petit-déjeuner",
+                  "Pizzas",
+                  "Plats préparés",
+                  "Produits laitiers",
+                  "Fromages",
+                  "Snacks sucrés",
+                  "Confiseries",
+                  "Sodas"]
