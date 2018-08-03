d6247e62d6c7473580105ffb5f557b357eb4cc75
Laure <lauremenaut@yahoo.fr>
2018-08-03 16:01:04 -0400

    add 'detail' view

--- pur_beurre_lm/healthier_food/views.py
+++ pur_beurre_lm/healthier_food/views.py
@@ -2,7 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+from .models import Product
 
 def home(request):
     template = loader.get_template('healthier_food/home.html')
     return HttpResponse(template.render(request=request))
+
+
+def detail(request, product_code):
+    code = int(product_code)
+    product = Product.objects.get(code=code)
+    template = loader.get_template('healthier_food/detail.html')
+    return HttpResponse(template.render({'product': product}, request=request))
