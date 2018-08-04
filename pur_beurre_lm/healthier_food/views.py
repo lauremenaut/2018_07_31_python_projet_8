d95d53450c3125196664c1bc2c740c23f39f377d
Laure <lauremenaut@yahoo.fr>
2018-08-04 00:11:40 -0400

    add 'search' view

--- pur_beurre_lm/healthier_food/views.py
+++ pur_beurre_lm/healthier_food/views.py
@@ -14,3 +14,18 @@ def detail(request, product_code):
     product = Product.objects.get(code=code)
     template = loader.get_template('healthier_food/detail.html')
     return HttpResponse(template.render({'product': product}, request=request))
+
+
+def search(request):
+    # L'utilisateur saisit un mot
+    # Le système lui propose des produits non sains qui correspondent à la recherche
+
+    query = request.GET.get('query')
+    unhealthy_products = Product.objects.filter(name__icontains=query).exclude(nutrition_grade="a").exclude(nutrition_grade="b")
+    template = loader.get_template('healthier_food/search.html')
+    return HttpResponse(template.render({'unhealthy_products': unhealthy_products}, request=request))
+
+
+
+    # L'utilisateur clique sur un des produits non sains
+    # Le système affiche le meilleur substitut
