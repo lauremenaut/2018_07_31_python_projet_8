31ba86277a88d5a705cccb0b2707e6c064c2281c
Laure <lauremenaut@yahoo.fr>
2018-09-05 13:46:34 -0400

    add 'substitute' route

--- pur_beurre_lm/healthier_food/urls.py
+++ pur_beurre_lm/healthier_food/urls.py
@@ -3,6 +3,7 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^(?P<product_code>[0-9]+)$', views.detail),
     re_path(r'^search/$', views.search),
+    re_path(r'^substitute/(?P<unhealthy_product_code>[0-9]+)$', views.substitute),
+    re_path(r'^(?P<product_code>[0-9]+)$', views.detail),
 ]
