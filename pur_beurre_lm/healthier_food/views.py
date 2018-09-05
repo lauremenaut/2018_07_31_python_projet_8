59d6f259fab53a65fca037fdde337fee1d234d57
Laure <lauremenaut@yahoo.fr>
2018-09-05 13:48:19 -0400

    split 'search' view into 'search' + 'substitute' views

--- pur_beurre_lm/healthier_food/views.py
+++ pur_beurre_lm/healthier_food/views.py
@@ -12,60 +12,61 @@ def home(request):
 
 def search(request):
     # L'utilisateur saisit un mot
-    # Le système lui propose des produits non sains qui correspondent à la recherche
-    # L'utilisateur clique sur un des produits non sains
-    # Le système affiche le meilleur substitut
+    # Le système lui propose des produits non sains qui correspondent à la recherche (6 max)
 
     query = request.GET.get('query')
-    unhealthy_products = Product.objects.filter(name__icontains=query).exclude(nutrition_grade="a").exclude(nutrition_grade="b")[:3]
-
-    healthy_products_dict = {}
-    unhealthy_healthy_products_dict = {}
-
-    for unhealthy_product in unhealthy_products:
-        healthy_products_list = []
-        categories = unhealthy_product.categories.all()
-        for category in categories:
-            healthy_products = Product.objects.filter(categories=category).exclude(nutrition_grade="d").exclude(nutrition_grade="e")
-            for healthy_product in healthy_products:
-                if healthy_product not in healthy_products_list:
-                    healthy_products_list.append(healthy_product)
-        for healthy_product in healthy_products_list:
-            shared_categories = []
-            for category in categories:
-                if Category.objects.get(name=category) in healthy_products_list:
-                    shared_categories.append(category)
-            healthy_products_dict[healthy_product] = len(shared_categories)
-
-        maximum = max(healthy_products_dict.values())
-
-        best_matches = []
-
-        for name, number_of_shared_categories in healthy_products_dict.items():
-            if number_of_shared_categories == maximum:
-                best_matches.append(name)
-
-        for match in best_matches:
-            healthiest_matches = Product.objects.filter(name=match).filter(nutrition_grade="a")
-
-        try:
-            if not healthiest_matches:
-                raise IndexError
-            else:
-                healthiest_match = healthiest_matches[randint(0, (len(healthiest_matches) - 1))]
-
-        except IndexError:
-            if not best_matches:
-                print("Aucun produit sain associé ... ne pas proposer le produit")
-
-            i = randint(0, (len(best_matches) - 1))
-            print("len(best_matches) : ", len(best_matches))
-            healthiest_match = best_matches[i]
-
-        unhealthy_healthy_products_dict[unhealthy_product] = healthiest_match.code
+    unhealthy_products = Product.objects.filter(name__icontains=query).exclude(nutrition_grade="a").exclude(nutrition_grade="b")[:6]
 
     template = loader.get_template('healthier_food/search.html')
-    return HttpResponse(template.render({'products_dict': unhealthy_healthy_products_dict}, request=request))
+    return HttpResponse(template.render({'unhealthy_products': unhealthy_products}, request=request))
+
+def substitute(request, unhealthy_product_code):
+    # L'utilisateur a cliqué sur un des produits non sains
+    # Le système affiche les meilleurs substituts (6 max)
+
+    unhealthy_product = Product.objects.get(code=unhealthy_product_code)
+
+    # Get healthy products sharing at least one category with unhealthy_product
+    healthy_products_list = []
+    categories = unhealthy_product.categories.all()
+    for category in categories:
+        healthy_products = Product.objects.filter(categories=category).exclude(nutrition_grade="d").exclude(nutrition_grade="e")
+        for healthy_product in healthy_products:
+            if healthy_product not in healthy_products_list:
+                healthy_products_list.append(healthy_product)
+
+    # Count shared categories between healthy and unhealthy products
+    shared_categories_counter = {}
+    for healthy_product in healthy_products_list:
+        shared_categories = []
+        for category in categories:
+            if Category.objects.get(name=category) in healthy_products:
+                shared_categories.append(category)
+        shared_categories_counter[healthy_product] = len(shared_categories)
+
+    maximum = max(shared_categories_counter.values())
+
+    best_matches = []
+
+    for healthy_product, number_of_shared_categories in shared_categories_counter.items():
+        if number_of_shared_categories == maximum and len(best_matches) < 6:
+            best_matches.append(healthy_product)
+
+    healthiest_matches_list = []
+
+    for match in best_matches:
+            healthiest_matches = Product.objects.filter(name=match).filter(nutrition_grade="a")[:6]
+            for healthiest_match in healthiest_matches:
+                healthiest_matches_list.append(healthiest_match)
+
+    i = 0
+    while len(healthiest_matches_list) < 6:
+        if best_matches[i] not in healthiest_matches_list:
+            healthiest_matches_list.append(best_matches[i])
+        i += 1
+
+    template = loader.get_template('healthier_food/substitute.html')
+    return HttpResponse(template.render({'unhealthy_product': unhealthy_product, 'healthy_products': healthiest_matches_list}, request=request))
 
 
 def detail(request, product_code):
