c6b184ea5fb5a1f212832cc6c62f5a74a11ce118
Laure <lauremenaut@yahoo.fr>
2018-09-06 15:58:42 -0400

    replace 'nutrition_grade' with 'nutriscore'

--- pur_beurre_lm/healthier_food/params.py
+++ pur_beurre_lm/healthier_food/params.py
@@ -1,5 +1,5 @@
 # Tags for nutrition grades of healthy and unhealthy products
-nutrition_grades = ["a", "b", "d", "e"]
+nutriscores = ["a", "b", "d", "e"]
 
 # Tags for (unhealthy) products categories
 tag_categories = ["Céréales pour petit-déjeuner",
