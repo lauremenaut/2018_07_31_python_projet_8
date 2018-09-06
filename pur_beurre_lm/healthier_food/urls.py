04f2e01d2503c7f61131c0fe3f5da6c4fb7f1d58
Laure <lauremenaut@yahoo.fr>
2018-09-05 23:35:40 -0400

    add 'name' attribute for each path

--- pur_beurre_lm/healthier_food/urls.py
+++ pur_beurre_lm/healthier_food/urls.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^search/$', views.search),
-    re_path(r'^substitute/(?P<unhealthy_product_code>[0-9]+)$', views.substitute),
-    re_path(r'^(?P<product_code>[0-9]+)$', views.detail),
+    re_path(r'^search/$', views.search, name='search'),
+    re_path(r'^substitute/(?P<unhealthy_product_code>[0-9]+)$', views.substitute, name='substitute'),
+    re_path(r'^(?P<product_code>[0-9]+)$', views.detail, name='detail'),
 ]
