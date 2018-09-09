c698a59a78ce80cb03790d90c93fc7a24d3cc2c1
Laure <lauremenaut@yahoo.fr>
2018-09-09 01:07:57 -0400

    add routes for 'new_account', 'success_account', 'login', 'my_account' - change route for 'detail'

--- pur_beurre_lm/healthier_food/urls.py
+++ pur_beurre_lm/healthier_food/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     re_path(r'^search/$', views.search, name='search'),
     re_path(r'^substitute/(?P<unhealthy_product_code>[0-9]+)$', views.substitute, name='substitute'),
-    re_path(r'^(?P<product_code>[0-9]+)$', views.detail, name='detail'),
+    re_path(r'^detail/(?P<product_code>[0-9]+)$', views.detail, name='detail'),
+    re_path(r'^new_account/$', views.new_account, name='new_account'),
+    re_path(r'^success_account/$', views.success_account, name='success_account'),
+    re_path(r'^login/$', views.login, name='login'),
+    re_path(r'^my_account/$', views.my_account, name='my_account'),
 ]
