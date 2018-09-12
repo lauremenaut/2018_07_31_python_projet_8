92b8c19e7ff72f5770e7dff686cec472f2664ebc
Laure <lauremenaut@yahoo.fr>
2018-09-11 23:11:23 -0400

    add new routes : login, logout, favorites, add_favorite, account_creation - replace 'detail' with 'details' route

--- pur_beurre_lm/healthier_food/urls.py
+++ pur_beurre_lm/healthier_food/urls.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
 from django.urls import re_path
 
 from . import views
@@ -5,9 +8,12 @@ from . import views
 urlpatterns = [
     re_path(r'^search/$', views.search, name='search'),
     re_path(r'^substitute/(?P<unhealthy_product_code>[0-9]+)$', views.substitute, name='substitute'),
-    re_path(r'^detail/(?P<product_code>[0-9]+)$', views.detail, name='detail'),
+    re_path(r'^details/(?P<product_code>[0-9]+)$', views.details, name='details'),
     re_path(r'^new_account/$', views.new_account, name='new_account'),
-    re_path(r'^success_account/$', views.success_account, name='success_account'),
-    re_path(r'^login/$', views.login, name='login'),
+    re_path(r'^account_creation/$', views.account_creation, name='account_creation'),
+    re_path(r'^login/$', views.login_user, name='login'),
     re_path(r'^my_account/$', views.my_account, name='my_account'),
+    re_path(r'^favorites/$', views.favorites, name='favorites'),
+    re_path(r'^add_favorite/(?P<favorite_code>[0-9]*)$', views.add_favorite, name='add_favorite'),
+    re_path(r'^logout/$', views.logout_user, name='logout'),
 ]
