5d6682f1bcbc880389888325a15c57652baf3300
Laure <lauremenaut@yahoo.fr>
2018-09-27 21:08:15 -0400

    add 'first_name' & 'last_name' fields

--- pur_beurre_lm/healthier_food/forms.py
+++ pur_beurre_lm/healthier_food/forms.py
@@ -1,22 +1,6 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-# from django import forms
-
-# class NewAccountForm(forms.Form):
-#     username = forms.CharField(
-#         label="Nom d'utilisateur",
-#         max_length=150,
-#         widget=forms.TextInput(attrs={'class': 'form-control'}),
-#         required=True
-#         )
-
-#     password = forms.Field(
-#         label="Mot de passe",
-#         widget=forms.TextInput(attrs={'class': 'form-control'}),
-#         required=True
-#         )
-
 from django.forms import ModelForm, TextInput, EmailInput, PasswordInput
 from django.contrib.auth.models import User
 
@@ -25,8 +9,10 @@ class NewAccountForm(ModelForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["first_name", "last_name", "username", "email", "password"]
         widgets = {
+            'first_name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex : Jean'}),
+            'last_name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex : Martin'}),
             'username': TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex : j.martin'}),
             'email': EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ex : jean.martin@email.com'}),
             'password': PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Ex : 32hg65ez98hj'})
