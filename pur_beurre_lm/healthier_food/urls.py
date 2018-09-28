3f1d8325f4849e75fadd0132bbb08285568044ab
Laure <lauremenaut@yahoo.fr>
2018-09-27 21:09:05 -0400

    replace 're_path()' with 'path()' method

--- pur_beurre_lm/healthier_food/urls.py
+++ pur_beurre_lm/healthier_food/urls.py
@@ -1,19 +1,19 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-from django.urls import re_path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    re_path(r'^search/$', views.search, name='search'),
-    re_path(r'^substitute/(?P<unhealthy_product_code>[0-9]+)/$', views.substitute, name='substitute'),
-    re_path(r'^details/(?P<product_code>[0-9]+)/$', views.details, name='details'),
-    re_path(r'^new_account/$', views.new_account, name='new_account'),
-    re_path(r'^account_creation/$', views.account_creation, name='account_creation'),
-    re_path(r'^login/$', views.login_user, name='login'),
-    re_path(r'^my_account/$', views.my_account, name='my_account'),
-    re_path(r'^favorites/$', views.favorites, name='favorites'),
-    re_path(r'^add_favorite/(?P<favorite_code>[0-9]*)/$', views.add_favorite, name='add_favorite'),
-    re_path(r'^logout/$', views.logout_user, name='logout'),
+    path('search', views.search, name='search'),
+    path('substitute/<int:unhealthy_product_code>', views.substitute, name='substitute'),
+    path('details/<int:product_code>', views.details, name='details'),
+    path('new_account', views.new_account, name='new_account'),
+    path('account_creation', views.account_creation, name='account_creation'),
+    path('login', views.login_user, name='login'),
+    path('my_account', views.my_account, name='my_account'),
+    path('favorites', views.favorites, name='favorites'),
+    path('add_favorite/<int:favorite_code>', views.add_favorite, name='add_favorite'),
+    path('logout', views.logout_user, name='logout'),
 ]
