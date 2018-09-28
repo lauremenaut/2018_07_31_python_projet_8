904666f209f55705639d9a31ebdb9a8bf3fda3eb
Laure <lauremenaut@yahoo.fr>
2018-09-27 21:13:31 -0400

    add 'ProductAdmin' class

--- pur_beurre_lm/healthier_food/admin.py
+++ pur_beurre_lm/healthier_food/admin.py
@@ -3,8 +3,16 @@
 
 from django.contrib import admin
 
-from .models import Product, Category, Favorite
+from .models import Product, Category, Favorite  #, PurBeurreUser
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'brand', 'nutriscore')
+    list_filter = ('nutriscore',)
+    search_fields = ('name', 'description', 'brand')
+    prepopulated_fields = {'slug': ('name', ), }
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
 admin.site.register(Favorite)
+# admin.site.register(PurBeurreUser)
