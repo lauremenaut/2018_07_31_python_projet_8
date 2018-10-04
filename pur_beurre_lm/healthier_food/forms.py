af915cc2acd5d0195365dbb690638114fc1e67c9
Laure <lauremenaut@yahoo.fr>
2018-10-04 14:09:36 -0400

    add 'LoginForm' form

--- pur_beurre_lm/healthier_food/forms.py
+++ pur_beurre_lm/healthier_food/forms.py
@@ -6,14 +6,22 @@ from django.contrib.auth.models import User
 
 
 class NewAccountForm(ModelForm):
-
     class Meta:
         model = User
         fields = ["first_name", "last_name", "username", "email", "password"]
         widgets = {
             'first_name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex : Jean'}),
             'last_name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex : Martin'}),
-            'username': TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex : j.martin'}),
             'email': EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ex : jean.martin@email.com'}),
+            'password': PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Ex : 32hg65ez98hj'})
+        }
+
+
+class LoginForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+        widgets = {
+            'username': TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex : jean.martin@email.com'}),
             'password': PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Ex : 32hg65ez98hj'})
         }
